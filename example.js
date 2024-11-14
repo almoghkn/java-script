bd4cbd4d5be8283661c7cd36c319f9ada8bf2eb3
Almog Hakun <almog7531@gmail.com>
2024-11-14 14:05:36 +0200

    My First Commit

---
+++ example.js
@@ -0,0 +1,24 @@
+// This is a print function
+// console.log("Hello World!")
+// let myName = 'Almog'
+// console.log(myName)
+
+// const myName = 'John'
+// //myName = 'Almog'
+// console.log(myName)
+
+// let myString = 'This is my first string'
+// console.log(typeof myString)
+
+//Object
+let student = {
+    name: 'Jonh',
+    age: 25,
+}
+
+console.log(student.name)
+
+//Array
+let students = ['John', 'Jane', 'Jack']
+
+console.log(students[1])
